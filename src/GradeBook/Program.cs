6e4b47e78e09cbf63822ce24c08883f6b3e2151d
Andrew Carnaghan <andreww92@live.com>
2021-03-08 11:09:21 +0000

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Andy's Study Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var stats = book.GetStats();
+            Console.WriteLine($"Average grade is {stats.Average:N1}");
+            Console.WriteLine($"Highest grade is {stats.High:N1}");
+            Console.WriteLine($"Lowest grade is {stats.Low:N1}");
+        }
+    }
+}
